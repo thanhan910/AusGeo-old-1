d079f61d63b0725404dd3d9bd6927c73eee0e9f1
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-02-20 19:58:39 +1100

    Update script, include downloading shp.zip to local folder

--- gen-mb-full.py
+++ gen-mb-full.py
@@ -2,8 +2,19 @@ import geopandas as gpd
 import pandas as pd
 import os
 import zipfile
+import requests
+import io
 
 url = 'https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files/MB_2021_AUST_SHP_GDA2020.zip'
+
+response = requests.get(url, stream=True)
+
+if response.status_code == 200:
+    
+    with zipfile.ZipFile(io.BytesIO(response.content)) as shp_zip:
+
+        # Save the shp_zip to a temporary folder
+        shp_zip.extractall('local')
 
 gdf : gpd.GeoDataFrame = gpd.read_file('local/MB_2021_AUST_SHP_GDA2020/MB_2021_AUST_GDA2020.shp')
 
