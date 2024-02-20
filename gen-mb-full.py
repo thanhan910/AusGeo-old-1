d4e86c1248950a89fe2e9059ca59f104b3036a6b
thanhan910 <63280785+thanhan910@users.noreply.github.com>
2024-02-20 19:45:37 +1100

    Full Mash Block shp dataset in geojson

---
+++ gen-mb-full.py
@@ -0,0 +1,12 @@
+import geopandas as gpd
+import pandas as pd
+import os
+import zipfile
+
+url = 'https://www.abs.gov.au/statistics/standards/australian-statistical-geography-standard-asgs-edition-3/jul2021-jun2026/access-and-downloads/digital-boundary-files/MB_2021_AUST_SHP_GDA2020.zip'
+
+gdf : gpd.GeoDataFrame = gpd.read_file('local/MB_2021_AUST_SHP_GDA2020/MB_2021_AUST_GDA2020.shp')
+
+os.makedirs('data/mb-geojson', exist_ok=True)
+
+gdf.groupby('SA4_CODE21').apply(lambda x: x.to_file(f'data/mb-geojson/MB-SA4-{x.name}.geojson', driver='GeoJSON'))
